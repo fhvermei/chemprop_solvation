1246dda7011d61f8cf3ca764794c85e096e0ca1c
Florence Vermeire <fhvermei@mit.edu>
2021-03-01 13:59:04 -0500

    Changed for conda package

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='chemprop_solvation',
     version='0.0.1',
-    author='Kyle Swanson, Kevin Yang, Wengong Jin',
-    author_email='swansonk@mit.edu, yangk@mit.edu, wengong@csail.mit.edu',
+    author='Kyle Swanson, Kevin Yang, Wengong Jin, Florence Vermeire',
+    author_email='swansonk@mit.edu, yangk@mit.edu, wengong@csail.mit.edu, fhvermei@mit.edu',
     description='Molecular Property Prediction with Message Passing Neural Networks',
-    url='https://github.com/swansonk14/chemprop',
+    url='https://github.com/fhvermei/chemprop_solvation',
     license='MIT',
     packages=find_packages(),
-    keywords=['chemistry', 'machine learning', 'property prediction', 'message passing neural network']
 )
